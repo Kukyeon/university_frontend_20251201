8fe41afb90313e694410e14dba07d008dff1ce5e
Kukyeon <rnrdus2000@naver.com>
2025-12-01 16:43:29 +0900

    axios 기본

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// 기본 Axios 인스턴스
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080", // 백엔드 기본 URL
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true, // 세션 인증이 필요할 경우
+});
