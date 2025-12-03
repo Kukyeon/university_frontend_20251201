c4f8e8637aece711fb21ba71907d9d52ee2886ef
LSM-1020 <smmlee@naver.com>
2025-12-03 16:38:36 +0900

    ChatAI 및 위험군확인

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Home from "./pages/Home";
 import Header from "./components/Home/Header";
 import Footer from "./components/Home/Footer";
 import { useState } from "react";
+import StudentMain from "./pages/StudentMain";   // 챗봇, 강의추천 있는 곳
+import ProfDashboard from "./pages/ProfDashboard"; // 위험군 대시보드
+import AdminPage from "./pages/AdminPage";       // 분석 실행 버튼
 
 function App() {
   const [user, setUser] = useState({
@@ -29,6 +32,16 @@ function App() {
         <Route path="/evaluation" element={<EvaluationPage />} />
         {/* 없는 경로는 home으로 redirect */}
         <Route path="/" element={<Navigate to="/" />} />
+        
+        {/* 챗봇 및 중도 이탈방지 관련부분 */}
+        {/* 학생이 로그인하면 들어가는 메인 화면 */}
+        <Route path="/student" element={<StudentMain />} />
+
+        {/* === [3] 교수용 (위험군 대시보드) === */}
+        <Route path="/professor" element={<ProfDashboard />} />
+
+        {/* === [4] 관리자용 (분석 실행) === */}
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
       <Footer />
     </div>
