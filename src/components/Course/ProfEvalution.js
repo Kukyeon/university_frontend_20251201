1c52114770d42e6c1cbd8924405d13a927e3c608
ddujeong <qoddl159@nate.com>
2025-12-09 15:48:12 +0900

    교수 강의 조회(내강의조회)

---
+++ src/components/Course/ProfEvalution.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfEvaluation = () => {
+  return (
+    <div className="my-evaluation-container">
+      <p className="no-data">조회할 강의 평가가 존재하지 않습니다.</p>
+    </div>
+  );
+};
+
+export default ProfEvaluation;
